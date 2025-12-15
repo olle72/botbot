10104f36dd572b2983beef323e663488502ea4f9
olle72 <oleg.bogatko@gmail.com>
2025-12-15 15:52:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,9 @@
-import os
-import logging
-from flask import Flask, request
-import telebot
-
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
-
-# Получаем токен из переменных окружения
-TOKEN = os.getenv('BOT_TOKEN')
-if not TOKEN:
-    raise ValueError("BOT_TOKEN не установлен!")
-
-bot = telebot.TeleBot(TOKEN)
+from flask import Flask
 app = Flask(__name__)
 
-# Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я работаю на Render!")
-
-# Веб-хук для Render
-@app.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-# Главный маршрут для проверки работы
 @app.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=os.getenv('WEBHOOK_URL') + '/' + TOKEN)
-    return "Бот запущен!", 200
+def hello():
+    return 'Бот работает!'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=5000)
